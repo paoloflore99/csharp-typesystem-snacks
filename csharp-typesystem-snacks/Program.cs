eba2403f5e34f1d7a993506fcf2a382dc227b0c1
paolo flore <paoloflore937@gmail.com>
2024-04-17 15:12:28 +0200

    orimo snack

--- csharp-typesystem-snacks/Program.cs
+++ csharp-typesystem-snacks/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace csharp_typesystem_snacks
+﻿using System;
+
+namespace csharp_typesystem_snacks
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            //punto uno
+            int numeroUno;
+            Console.WriteLine("inserisci il primo numero");
+            numeroUno = Convert.ToInt32(Console.ReadLine());
+
+            int numeroDue;
+            Console.WriteLine("inserisci il secondo numero");
+            numeroDue = Convert.ToInt32(Console.ReadLine());
+
+            if (numeroDue > numeroUno)
+            {
+                Console.WriteLine($"il numero più grande è il {numeroDue}");
+            }
+            else if (numeroDue < numeroUno)
+            {
+                Console.WriteLine($"il numero più grande è il {numeroUno}");
+            }
+            else
+            {
+                Console.WriteLine("i numeri sono pari");
+            }
+
+
+
+
+
+
+
         }
     }
 }
+
